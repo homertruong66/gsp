e3a54ae7c9b33d2919804d522cc2ccda6fde6c5f
Cuong Phan <cuongphan97@gmail.com>
2019-11-30 22:02:33 +0700

    setup gsp's authentication

---
+++ src/main/java/com/gsp/gsp/models/User.java
@@ -0,0 +1,81 @@
+package com.gsp.gsp.models;
+
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String pass;
+    private byte role;
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public byte getRole() {
+        return role;
+    }
+
+    public void setRole(byte role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", pass='" + pass + '\'' +
+                ", role=" + role +
+                ", token='" + token + '\'' +
+                '}';
+    }
+}
