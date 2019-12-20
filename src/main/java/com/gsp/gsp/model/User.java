e3545f3c4b580ae1b59dfc2f2ae6f97ef8aa40d1
Dung <dung.nguyen.tien.0810@gmail.com>
2019-12-20 21:50:19 +0700

    Add Delete User
    Logical errors are not treated

--- src/main/java/com/gsp/gsp/model/User.java
+++ src/main/java/com/gsp/gsp/model/User.java
@@ -10,6 +10,17 @@ public class User {
     private String role;
     private String token;
 
+    public User(){}
+    public User(String id, String firstName, String lastName, String email, String pass, String role, String token) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.pass = pass;
+        this.role = role;
+        this.token = token;
+    }
+
     public String getToken() {
         return token;
     }
